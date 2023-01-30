d812269cfb9d2fd825494afd700c7a13188d4c62
leandro-miranda <leandromiranda2010@gmail.com>
2023-01-29 23:53:01 -0300

    create e styled Footer

---
+++ mks-frontend-challenge/src/components/Footer/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.div`
+  width: 100%;
+  height: 34px;
+  background-color: #EEEEEE;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 12px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+`;
