486fd1248d483a9ee7a302a7ae7d5d7a1c3264e7
leandro-miranda <leandromiranda2010@gmail.com>
2023-01-29 23:53:11 -0300

    create e styled Footer

---
+++ mks-frontend-challenge/src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import { FooterContainer } from './style';
+
+export function Footer() {
+  return (
+    <FooterContainer>
+      MKS sistemas © Todos os direitos reservados
+    </FooterContainer>
+  )
+}
